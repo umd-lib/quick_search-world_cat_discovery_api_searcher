1ecb6994d192bf6a348a30f2c47ca097c824bac0
Mohamed Abdul Rasheed <mohideen@umd.edu>
2018-06-14 12:04:25 -0400

    LIBSEARCH-46. Initial implementation.
    https://issues.umd.edu/browse/LIBSEARCH-46

--- app/searchers/quick_search/world_cat_discovery_api_searcher.rb
+++ app/searchers/quick_search/world_cat_discovery_api_searcher.rb
@@ -1,1 +1,53 @@
-# WorldCat Discovery Searcher Implementation
+# frozen_string_literal: true
+
+module QuickSearch
+  # QuickSearch seacher for WorldCat
+  class WorldCatDiscoveryApiSearcher < QuickSearch::Searcher
+    def search
+      @response = WorldCat::Discovery::Bib.search(query_params)
+    end
+
+    def results # rubocop:disable Metrics/MethodLength, Metrics/AbcSize
+      return results_list[0..@per_page - 1] if results_list
+      @results_list = []
+      @response.bibs.each do |bib|
+        result = OpenStruct.new
+        result.title = bib.name
+        result.link = item_link(bib)
+        result.author = bib.author.try(:name)
+        result.date = bib.date_published
+        @results_list << result
+      end
+      @results_list[0..@per_page - 1]
+    end
+
+    def total
+      @response.total_results
+    end
+
+    def query_params
+      {
+        q: http_request_queries['not_escaped'],
+        startIndex: @offset,
+        itemsPerPage: items_per_page
+      }
+    end
+
+    def loaded_link
+      QuickSearch::Engine::WORLD_CAT_DISCOVERY_API_CONFIG['loaded_link'] +
+        http_request_queries['uri_escaped']
+    end
+
+    def item_link(bib)
+      loaded_link + '#/oclc/' + bib.oclc_number.to_s
+    end
+
+    def items_per_page
+      allowed_values = [10, 25, 50, 100]
+      allowed_values.each do |val|
+        return val if @per_page <= val
+      end
+      allowed_values.last
+    end
+  end
+end
--- config/initializers/world_cat_discovery_api_config.rb
+++ config/initializers/world_cat_discovery_api_config.rb
@@ -7,3 +7,19 @@ else
   config_file = File.expand_path('../../world_cat_discovery_api_config.yml', __FILE__)
 end
 QuickSearch::Engine::WORLD_CAT_DISCOVERY_API_CONFIG = YAML.load(ERB.new(File.read(config_file)).result)[Rails.env]
+
+# Get the configuration values
+key = QuickSearch::Engine::WORLD_CAT_DISCOVERY_API_CONFIG['wskey']
+secret = QuickSearch::Engine::WORLD_CAT_DISCOVERY_API_CONFIG['secret']
+authenticating_institution_id = QuickSearch::Engine::WORLD_CAT_DISCOVERY_API_CONFIG['authenticatingInstitutionId']
+context_institution_id = QuickSearch::Engine::WORLD_CAT_DISCOVERY_API_CONFIG['contextInstitutionId']
+
+# Create WSKey object
+wskey = OCLC::Auth::WSKey.new(key, secret, :services => ['WorldCatDiscoveryAPI'])
+
+# Configure WorldCat Discovery gem
+WorldCat::Discovery.configure(wskey, authenticating_institution_id, context_institution_id)
+
+# Set SSL version to TLSv1_2. Otherwise SSLv3 is used as default and results in a handshake error
+# Error will be triggered at https://github.com/OCLC-Developer-Network/oclc-auth-ruby/blob/master/lib/oclc/auth/access_token.rb#L58
+OpenSSL::SSL::SSLContext::DEFAULT_PARAMS[:ssl_version] = 'TLSv1_2'
