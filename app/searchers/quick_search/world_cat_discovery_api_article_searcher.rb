967f71497b1eccd3babf6566184a9f46e7f17f2e
David P. Steelman <dsteelma@umd.edu>
2019-12-10 09:40:45 -0500

    LIBSEARCH-74. Added "umd_open_url" gem dependency
    Added "umd_open_url" gem dependency, and updated code to use OpenUrl
    functionality.
    
    https://issues.umd.edu/browse/LIBSEARCH-74

--- app/searchers/quick_search/world_cat_discovery_api_article_searcher.rb
+++ app/searchers/quick_search/world_cat_discovery_api_article_searcher.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require 'umd_open_url'
+
 module QuickSearch
   # QuickSearch searcher for WorldCat, restricted to articles
   class WorldCatDiscoveryApiArticleSearcher < WorldCatDiscoveryApiSearcher
@@ -24,11 +26,17 @@ module QuickSearch
     def item_link(bib)
       doi_link = bib.same_as&.to_s
 
+      # Return DOI link, if available
       if doi_link
         doi_base_url = QuickSearch::Engine::WORLD_CAT_DISCOVERY_API_ARTICLE_CONFIG['doi_link']
         return doi_base_url + doi_link
       end
 
+      # Return link WorldCat OpenUrl link resolver, if available
+      open_url_link = link_from_open_url(bib)
+      return open_url_link if open_url_link
+
+      # Otherwise just return link to OCLC catalog
       QuickSearch::Engine::WORLD_CAT_DISCOVERY_API_ARTICLE_CONFIG['url_link'] +
         bib.oclc_number.to_s
     end
@@ -43,5 +51,49 @@ module QuickSearch
     def item_format(_bib)
       'article'
     end
+
+    def link_from_open_url(bib)
+      # Generate the link to the WorldCat OpenUrl Resolver
+      open_url_link = open_url_resolve_link(bib)
+      json = UmdOpenUrl::Resolver.resolve(open_url_link)
+      link = UmdOpenUrl::Resolver.parse_response(json)
+
+      link
+    end
+
+    def open_url_resolve_link(bib) # rubocop:disable Metrics/AbcSize, Metrics/CyclomaticComplexity, Metrics/PerceivedComplexity, Metrics/MethodLength
+      best_type = WorldCat::Discovery::Bib.choose_best_type(bib)
+      return nil unless best_type.is_a? WorldCat::Discovery::Article
+
+      article = best_type
+
+      # WorldCat code doesn't always check for nil, so wrap in begin/rescue
+      # so that we can just return nil if an error occurs.
+      begin
+        issn = article&.periodical&.issn
+        volume = article&.volume&.volume_number
+        issue_number = article&.issue&.issue_number
+        page_start = article&.page_start
+        date_published = article&.date_published
+      rescue StandardError
+        return nil
+      end
+
+      # Return nil if the necessary parameters weren't found.
+      return nil unless issn && volume && issue_number && page_start && date_published
+
+      open_url_resolver_service_link =
+        QuickSearch::Engine::WORLD_CAT_DISCOVERY_API_ARTICLE_CONFIG['open_url_resolver_service_link']
+      open_url_wskey = QuickSearch::Engine::WORLD_CAT_DISCOVERY_API_ARTICLE_CONFIG['world_cat_open_url_wskey']
+
+      b = UmdOpenUrl::Builder.new(open_url_resolver_service_link)
+      b.custom_param('wskey', open_url_wskey).issn(issn).volume(volume).start_page(page_start)
+       .publication_date(date_published)
+
+      url = b.build
+
+      url
+    end
   end
 end
+
