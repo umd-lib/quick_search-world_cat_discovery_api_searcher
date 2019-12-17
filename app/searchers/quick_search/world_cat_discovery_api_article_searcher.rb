797e98e3a1c5a4cba4584cb1750da243c874fcd2
David P. Steelman <dsteelma@umd.edu>
2019-12-17 08:14:05 -0500

    LIBSEARCH-74. Modified to handle multiple URLS and citation finder
    Modified to handle multiple URLs from umd_open_url gem, and modified
    to use citation finder if more than one URL is returned. If a single
    URL is returned, it is used as the result.
    
    https://issues.umd.edu/browse/LIBSEARCH-74

--- app/searchers/quick_search/world_cat_discovery_api_article_searcher.rb
+++ app/searchers/quick_search/world_cat_discovery_api_article_searcher.rb
@@ -23,7 +23,7 @@ module QuickSearch
     # Returns the link to use for the given item. If the item has a DOI
     # a direct link to the item is returned, otherwise a link to the
     # item in the OCLC catalog is returned.
-    def item_link(bib) # rubocop:disable Metrics/AbcSize, Metrics/MethodLength
+    def item_link(bib) # rubocop:disable Metrics/MethodLength
       doi_link = bib.same_as&.to_s
 
       # Return DOI link, if available
@@ -34,10 +34,12 @@ module QuickSearch
       end
 
       # Return link WorldCat OpenUrl link resolver, if available
-      open_url_link = link_from_open_url(bib)
-      if open_url_link
-        Rails.logger.debug('QuickSearch::WorldCatDiscoveryApiArticleSearcher.item_link - OpenURL link found. Returning.')
-        return open_url_link
+      link_from_open_url = link_from_open_url(bib)
+      if link_from_open_url
+        Rails.logger.debug(
+          'QuickSearch::WorldCatDiscoveryApiArticleSearcher.item_link - OpenURL link found. Returning.'
+        )
+        return link_from_open_url
       end
 
       # Otherwise just return link to catalog detail page
@@ -61,9 +63,41 @@ module QuickSearch
       # Generate the link to the WorldCat OpenUrl Resolver
       open_url_link = open_url_resolve_link(bib)
       json = UmdOpenUrl::Resolver.resolve(open_url_link)
-      link = UmdOpenUrl::Resolver.parse_response(json)
+      links = UmdOpenUrl::Resolver.parse_response(json)
 
-      link
+      return nil if links.nil? || links.size.zero?
+
+      if links.size == 1
+        Rails.logger.debug(
+          'QuickSearch::WorldCatDiscoveryApiArticleSearcher.link_from_open_url - '\
+          'Single OpenURL resolved link found. Returning.'
+        )
+        return link[0]
+      else
+        Rails.logger.debug(
+          'QuickSearch::WorldCatDiscoveryApiArticleSearcher.link_from_open_url - '\
+          "#{links.size} OpenURL resolved links found. Returning link to citation finder"
+        )
+        open_url_link_uri = URI.parse(open_url_link)
+        params_map = CGI.parse(open_url_link_uri.query)
+        filtered_params_map = params_map.reject { |k, _v| k == 'wskey' }
+
+        # Regenerate the query parameters string. Using Rack::Utils.build_query
+        # because it produces a query string without array-based parameters
+        filtered_params = Rack::Utils.build_query(filtered_params_map)
+
+        filtered_params = nil if filtered_params.strip.empty?
+
+        # Construct the link to the resource
+        citiation_finder_uri = URI::HTTP.build(
+          host: 'umaryland.on.worldcat.org',
+          path: '/atoztitles/link',
+          query: filtered_params
+        )
+        citiation_finder_uri.scheme = 'https'
+        citiation_finder_url = citiation_finder_uri.to_s
+        citiation_finder_url
+      end
     end
 
     def open_url_resolve_link(bib) # rubocop:disable Metrics/AbcSize, Metrics/CyclomaticComplexity, Metrics/PerceivedComplexity, Metrics/MethodLength
@@ -84,18 +118,23 @@ module QuickSearch
         return nil
       end
 
+      Rails.logger.debug do
+        <<~LOGGER_END
+          QuickSearch::WorldCatDiscoveryApiArticleSearcher.open_url_resolve_link
+          \tissn: #{issn}
+          \tvolume: #{volume}
+          \tissue_number: #{issue_number}
+          \tpage_start: #{page_start}
+          \tdate_published: #{date_published}
+        LOGGER_END
+      end
+
       # Return nil if the necessary parameters weren't found.
       unless issn && volume && issue_number && page_start && date_published
-        Rails.logger.debug{
-          <<~LOGGER_END
-            QuickSearch::EbscoDiscoveryServiceApiArticleSearcher.open_url_resolve_link data missing -
-            \tissn: #{issn}
-            \tvolume: #{volume}
-            \tissue_number: #{issue_number}
-            \tpage_start: #{page_start}
-            \tdate_published: #{date_published}
-          LOGGER_END
-        }
+        Rails.logger.debug do
+          'QuickSearch::WorldCatDiscoveryApiArticleSearcher.open_url_resolve_link data missing. Returning nil'
+        end
+
         return nil
       end
 
@@ -113,4 +152,3 @@ module QuickSearch
     end
   end
 end
-
